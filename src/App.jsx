13e4ef780b56bc675df98cd529acfdf99186706e
Tasin-Tausif <tasin@magnetismtech.com>
2025-12-01 12:48:12 +0600

    Env Ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
 export default function App() {
+ // When the app loads, it will load the env variable only once
+//  Must restart the app if any changes were made in the env file
 
+ // if project is created with create react(npx), value of env is fetched in this way
+ 	// console.log(process.env.REACT_APP_APPWRITE_URL)
+
+ // if project is created with vite, value of env is fetched in this way
+	// console.log(import.meta.env.VITE_APPWRITE_URL)	
   return (
     <>
       
---
+++ src/config/conf.js
@@ -0,0 +1,11 @@
+// Since all the env variables are required in string format and for easy access, they are loaded here
+const conf = {
+    appwriteUrl: String(import.meta.env.VITE_APPWRITE_URL),
+    appwriteProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+    appwriteProjectName: String(import.meta.env.VITE_APPWRITE_PROJECT_NAME),
+    appwriteDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
+    appwriteTableId: String(import.meta.env.VITE_APPWRITE_TABLE_ID),
+    appwriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID)
+}
+
+export default conf
