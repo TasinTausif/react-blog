358420b42da1fd00fb668b411829b37a035b0579
Tasin-Tausif <tasin@magnetismtech.com>
2025-12-03 17:52:21 +0600

    WIP

---
+++ src/components/PostCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import postService from "../appwrite/post"
+import { Link } from 'react-router-dom'
+
+export default function PostCard({$id, title, featuredImage}) {
+  return (
+    <Link to={``}>
+        
+    </Link>
+  )
+}
---
+++ src/components/Select.jsx
@@ -0,0 +1,34 @@
+import { useId } from 'react'
+
+const Select = ({
+    label,
+    options = [],
+    className = "",
+    ref,
+    ...props
+}) => {
+    const id = useId()
+
+    return (
+        <div className="w-full">
+            {label && <label className='' htmlFor={id}>{label}</label>}
+            <select
+                id={id}
+                className={`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full ${className}`}
+                ref={ref}
+                {...props}
+            >
+                {options?.map(option => (
+                    <option
+                        key={option}
+                        value={option}
+                    >
+                        {option}
+                    </option>
+                ))}
+            </select>
+        </div>
+    )
+}
+
+export default Select
