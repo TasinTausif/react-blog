0a5f49cfd90e0e26969e7742b377292dc40eeda2
TasinTausif <tasintausif@gmail.com>
2025-12-03 23:05:52 +0600

    Login Component

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
-import postService from "../appwrite/post"
+import fileService from "../appwrite/file"
 import { Link } from 'react-router-dom'
 
-export default function PostCard({$id, title, featuredImage}) {
-  return (
-    <Link to={``}>
-        
-    </Link>
-  )
+// In appwrite, id is passed in this way $id
+export default function PostCard({ $id, title, featuredImage }) {
+	return (
+		// Link's url starts from the current position, so we don't need to give full path
+		<Link to={`/posts/${$id}`}>
+			<div className='w-full bg-gray-100 rounded-xl p-4'>
+				<div className='w-full justify-center mb-4'>
+					<img
+						src={fileService.getFilePreview(featuredImage)}
+						alt={title}
+						className='rounded-xl'
+					/>
+				</div>
+				<h2 className='text-xl font-bold'>
+					{title}
+				</h2>
+			</div>
+		</Link>
+	)
 }
--- src/components/index.js
+++ src/components/index.js
@@ -3,11 +3,21 @@ import Footer from "./footer/Footer";
 import Logo from "./Logo";
 import LogoutBtn from "./header/LogoutBtn";
 import Container from "./container/Container";
+import Input from "./Input"
+import Button from "./Button"
+import Login from "./Login"
+import PostCard from "./PostCard"
+import Select from "./Select"
 
 export {
     Header, 
     Footer,
     Logo,
     LogoutBtn,
-    Container
+    Container,
+    Input,
+    Button,
+    Login,
+    PostCard,
+    Select
 }
