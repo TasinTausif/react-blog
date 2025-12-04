634a762ecfdace32335573eb019f5a61d93ac239
TasinTausif <tasintausif@gmail.com>
2025-12-04 23:00:44 +0600

    Form and slug func done

--- src/components/post-form/PostForm.jsx
+++ src/components/post-form/PostForm.jsx
@@ -1,4 +1,4 @@
-import { useCallback } from 'react'
+import { useCallback, useEffect } from 'react'
 import { useForm } from 'react-hook-form'
 import { Button, Input, Select, RTE } from "../"
 import { useNavigate } from 'react-router-dom'
@@ -6,35 +6,127 @@ import { useSelector } from 'react-redux'
 import postService from "../../appwrite/post"
 import fileService from "../../appwrite/file"
 
-export default function PostForm({post}) {
+export default function PostForm({ post }) {
     const userData = useSelector(state => state.auth.userData)
     const navigate = useNavigate()
     // Watch will monitor the field continuesly. In react form, values are set as setValue. And getValues will give me the valuesm of the form fields
     // Inside the form, we can set default values too
     const { register, handleSubmit, watch, setValue, control, getValues } = useForm({
         defaultValues: {
-            title: post?.title || "", 
-            slug: post?.slug || "", 
-            content: post?.content || "", 
-            status: post?.status || "active", 
+            title: post?.title || "",
+            slug: post?.slug || "",
+            content: post?.content || "",
+            status: post?.status || "active",
         }
     })
 
-    const submit = async(data) => {
-        if(post){
-            
-        }else{
+    const submit = async (data) => {
+        if (post) {
+            // Since it's updating, upload new image here
+            const file = data.image[0] ? await fileService.uploadFile(data.image[0]) : null
 
+            // Removing old imgage
+            if (file) {
+                await fileService.deleteFile(post.featuredImage)
+            }
+
+            const dbPost = await postService.updatePost(post.$id, {
+                ...data,
+                featuredImage: file ? file.$id : undefined
+            })
+
+            if (dbPost) {
+                navigate(`post/${dbPost.$id}`)
+            }
+        } else {
+            const file = await fileService.uploadFile(data.image[0])
+
+            data.featuredImage = file.$id
+
+            const dbPost = await postService.createPost({
+                ...data,
+                userId: userData.$id
+            })
+
+            if (dbPost) {
+                navigate(`post/${dbPost.$id}`)
+            }
         }
     }
 
-    useCallback(() => {
+    const slugTransform = useCallback((value) => {
+        if (value && typeof value === 'string')
+            return value
+                .trim()
+                .toLowerCase()
+                .replace(/[^a-zA-Z\d\s]+/g, "-")
+                .replace(/\s/g, "-");
 
+        return ""
     }, [])
 
-    return (
-        <div>
+    // To optimize a useEffect, store the func in a var and in the return of the useEffect, call a callback in the return and mention that var with a unsubscribe(), so that it doesn't loop over and over inside the useEffect
+    useEffect(() => {
+        const subscription = watch((value, { name }) => {
+            if (name === "title") {
+                setValue("slug", slugTransform(value.title,
+                    { shouldValidate: true }))
+            }
+        })
 
-        </div>
+        return () => {
+            subscription.unsubscribe()
+        }
+    }, [watch, slugTransform, setValue])
+
+
+    return (
+        <form onSubmit={handleSubmit(submit)} className="flex flex-wrap">
+            <div className="w-2/3 px-2">
+                <Input
+                    label="Title :"
+                    placeholder="Title"
+                    className="mb-4"
+                    {...register("title", { required: true })}
+                />
+                <Input
+                    label="Slug :"
+                    placeholder="Slug"
+                    className="mb-4"
+                    {...register("slug", { required: true })}
+                    onInput={(e) => {
+                        setValue("slug", slugTransform(e.currentTarget.value), { shouldValidate: true });
+                    }}
+                />
+                <RTE label="Content :" name="content" control={control} defaultValue={getValues("content")} />
+            </div>
+            <div className="w-1/3 px-2">
+                <Input
+                    label="Featured Image :"
+                    type="file"
+                    className="mb-4"
+                    accept="image/png, image/jpg, image/jpeg, image/gif"
+                    {...register("image", { required: !post })}
+                />
+                {post && (
+                    <div className="w-full mb-4">
+                        <img
+                            src={appwriteService.getFilePreview(post.featuredImage)}
+                            alt={post.title}
+                            className="rounded-lg"
+                        />
+                    </div>
+                )}
+                <Select
+                    options={["active", "inactive"]}
+                    label="Status"
+                    className="mb-4"
+                    {...register("status", { required: true })}
+                />
+                <Button type="submit" bgColor={post ? "bg-green-500" : undefined} className="w-full">
+                    {post ? "Update" : "Submit"}
+                </Button>
+            </div>
+        </form>
     )
 }
